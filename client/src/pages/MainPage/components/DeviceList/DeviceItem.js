1bc910e62f01cebab85f12ffcb2c4f0ace4e85de
ktsudikov <ktsudikov@conceptsoft.io>
2022-03-13 20:03:01 +0200

    device item fix

--- client/src/pages/MainPage/components/DeviceList/DeviceItem.js
+++ client/src/pages/MainPage/components/DeviceList/DeviceItem.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import {Button, Card, Col, Image} from 'react-bootstrap';
+import {Card, Col, Image} from 'react-bootstrap';
 import {useHistory} from 'react-router-dom';
 import {REACT_APP_API_URL} from '../../../../http/api';
 import {DEVICE_ROUTE} from '../../../../utils/constants';
-import RatingStar from "../../../../components/Rating/RatingStar";
+import RatingStar from '../../../../components/Rating/RatingStar';
 import * as styles from './Device.module.scss';
 
-const DeviceItem = ({device, deleteItem}) => {
+const DeviceItem = ({device}) => {
   const history = useHistory()
+
   return (
-    <Col md={3} className={"mt-3"}>
-      <Card border="light">
-        <Image
-          className={styles.img}
-          src={REACT_APP_API_URL + device.img}
-          onClick={() => history.push(DEVICE_ROUTE + '/' + device.id)}
-        />
+    <Col md={3} className={'mt-3'}>
+      <Card
+        border="light"
+        className={styles.imgContainer}
+        onClick={() => history.push(DEVICE_ROUTE + '/' + device.id)}
+      >
+        <div>
+          <Image
+            className={styles.img}
+            src={REACT_APP_API_URL + device.img}
+          />
+        </div>
         <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
           <div className={styles.price}>{device.price} грн</div>
           <div className="d-flex align-items-center">
@@ -25,17 +31,6 @@ const DeviceItem = ({device, deleteItem}) => {
 
         </div>
         <div>{device.name}</div>
-        {deleteItem && <Button
-          className="mx-3"
-          variant="outline-dark"
-          onClick={(e) => {
-            e.preventDefault()
-            deleteItem(device.id, e)
-          }
-          }
-        >
-          Delete
-        </Button>}
       </Card>
     </Col>
   );
